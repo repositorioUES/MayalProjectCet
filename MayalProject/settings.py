931e1698580c2f1cf870328106236e457fff1fd4
redy-commits <santoschavezn@gmail.com>
2022-10-23 23:12:54 -0600

    Cambio de nombre temporal.

--- MayalProject/settings.py
+++ MayalProject/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'MayalProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
